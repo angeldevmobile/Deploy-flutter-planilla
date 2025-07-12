e5ecc461c0b2cb9318358590ff7f913acc62c2a7
Gabriel Zapata <zapata.axuariogabriel@gmail.com>
2025-07-12 14:31:21 -0500

    nuevos cambios en front

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2830839906"
+    serviceWorkerVersion: "144474079"
   }
 });
